880e9f493c077227d3b712041ccab17ea2ef14e2
donggunseo <donggunseo@naver.com>
2025-04-10 13:36:26 +0900

    20250410

--- exp3.sh
+++ exp3.sh
@@ -1,19 +1,23 @@
 subset=(banking77 clinc150 trec)
 
 for subset_name in "${subset[@]}"; do
-    CUDA_VISIBLE_DEVICES=2 python3 Few_shot.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results/no_inst --n_shots 100 --max_new_tokens 10 --insert_inst
-    CUDA_VISIBLE_DEVICES=2 python3 Zero_shot.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results/no_inst --max_new_tokens 10 --insert_inst
-    CUDA_VISIBLE_DEVICES=2 python3 Diff_icv.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results/no_inst --n_shots 100 --max_new_tokens 10 --insert_inst
+    CUDA_VISIBLE_DEVICES=2 python3 Few_shot.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_7b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-7b-hf
+    CUDA_VISIBLE_DEVICES=2 python3 Zero_shot.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_7b --max_new_tokens 10 --model_name meta-llama/Llama-2-7b-hf
+    CUDA_VISIBLE_DEVICES=2 python3 Diff_icv.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_7b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-7b-hf
 done
-
-CUDA_VISIBLE_DEVICES=2 python3 Few_shot.py --dataset_name wmt19 --seed 40 --save_path_root ./results/no_inst --n_shots 50 --max_new_tokens 200 --insert_inst
-CUDA_VISIBLE_DEVICES=2 python3 Zero_shot.py --dataset_name wmt19 --seed 40 --save_path_root ./results/no_inst --max_new_tokens 200 --insert_inst
-CUDA_VISIBLE_DEVICES=2 python3 Diff_icv.py --dataset_name wmt19 --seed 40 --save_path_root ./results/no_inst --n_shots 50 --max_new_tokens 200 --insert_inst
 
 for subset_name in "${subset[@]}"; do
-    CUDA_VISIBLE_DEVICES=2 python3 Task_vector.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results/no_inst --n_shots 100 --max_new_tokens 10 --insert_inst
-    CUDA_VISIBLE_DEVICES=2 python3 Diff_icv_baseline.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results/no_inst --n_shots 100 --max_new_tokens 10 --insert_inst
+    CUDA_VISIBLE_DEVICES=2 python3 Task_vector.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_7b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-7b-hf
+    CUDA_VISIBLE_DEVICES=2 python3 Diff_icv_baseline.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_7b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-7b-hf
 done
 
-CUDA_VISIBLE_DEVICES=2 python3 Task_vector.py --dataset_name wmt19 --seed 40 --save_path_root ./results/no_inst --n_shots 50 --max_new_tokens 200 --insert_inst
-CUDA_VISIBLE_DEVICES=2 python3 Diff_icv_baseline.py --dataset_name wmt19 --seed 40 --save_path_root ./results/no_inst --n_shots 50 --max_new_tokens 200 --insert_inst
+for subset_name in "${subset[@]}"; do
+    CUDA_VISIBLE_DEVICES=2 python3 Few_shot.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_13b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-13b-hf
+    CUDA_VISIBLE_DEVICES=2 python3 Zero_shot.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_13b --max_new_tokens 10 --model_name meta-llama/Llama-2-13b-hf
+    CUDA_VISIBLE_DEVICES=2 python3 Diff_icv.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_13b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-13b-hf
+done
+
+for subset_name in "${subset[@]}"; do
+    CUDA_VISIBLE_DEVICES=2 python3 Task_vector.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_13b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-13b-hf
+    CUDA_VISIBLE_DEVICES=2 python3 Diff_icv_baseline.py --dataset_name "$subset_name" --seed 40 --save_path_root ./results_llama2_13b --n_shots 100 --max_new_tokens 10 --model_name meta-llama/Llama-2-13b-hf
+done
